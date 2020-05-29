543d0611a460f514a4e80af8817f2a83a4184f23
David Lee <davidsamlee@gmail.com>
2020-05-29 22:21:14 +0000

    Fixed formatting, added debug message.

--- questions.py
+++ questions.py
@@ -1,12 +1,16 @@
 from moviedata import getRandomMovies
+import json
 
 # Simple example function to generate a random multiple choice movie question with three answers.
 def questionAndAnswers():
     film = getRandomMovies(1)[0] # Pick a single movie at random
+    
+    # Print the data to the console for debugging purposes.
+    print(json.dumps(film))
+    
     yearReleased = int(film["release_date"][:4]) # Get the release date, take the first 4 characters (the year), and convert from string to a number
     return {
-        "question":
-        "Which year was the movie {} released?".format(film["title"]),
+        "question": "Which year was the movie {} released?".format(film["title"]),
         "answers": [yearReleased - 2, yearReleased, yearReleased + 2],
         "rightAnswerIndex": 1
     }
